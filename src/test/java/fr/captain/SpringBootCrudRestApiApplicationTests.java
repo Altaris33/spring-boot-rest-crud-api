16c3935ede447bbac92807e3977d958d9e665937
altaris33 <jeremie.laera@live.fr>
2026-05-14 18:32:47 +0200

    initial commit: created a REST controller for returning a Hello World String over HTTP

---
+++ src/test/java/fr/captain/SpringBootCrudRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.captain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
