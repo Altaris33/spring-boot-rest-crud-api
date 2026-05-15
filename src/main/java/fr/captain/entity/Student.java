143277c3cfc5a242889a4372b8e03e89b65dd5ee
altaris33 <jeremie.laera@live.fr>
2026-05-15 19:07:34 +0200

    Implemented a REST endpoint to get a resource by id & refactored the controller to create student hard-coded data only once

--- src/main/java/fr/captain/entity/Student.java
+++ src/main/java/fr/captain/entity/Student.java
@@ -2,15 +2,25 @@ package fr.captain.entity;
 
 public class Student {
 
+    private int id;
     private String firstName;
     private String lastName;
 
     public Student() {
     }
 
-    public Student(String firstName, String lastName) {
+    public Student(int id, String firstName, String lastName) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getFirstName() {
--- src/main/java/fr/captain/rest/StudentController.java
+++ src/main/java/fr/captain/rest/StudentController.java
@@ -1,7 +1,9 @@
 package fr.captain.rest;
 
 import fr.captain.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +14,24 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentController {
 
+    private List<Student> students;
+
     @GetMapping("/students")
     public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
-
-        students.add(new Student("Captain", "Falcon"));
-        students.add(new Student("Ganon", "Dorf"));
-        students.add(new Student("Rean", "Scharzer"));
-        students.add(new Student("Alisa", "Reinford"));
-
         return students;
+    }
+
+    @GetMapping("/students/{id}")
+    public Student getStudentById(@PathVariable int id) {
+        return students.get(id);
+    }
+
+    @PostConstruct
+    public void loadStudents() {
+        students = new ArrayList<>();
+        students.add(new Student(1, "Captain", "Falcon"));
+        students.add(new Student(2, "Ganon", "Dorf"));
+        students.add(new Student(3, "Rean", "Scharzer"));
+        students.add(new Student(4, "Alisa", "Reinford"));
     }
 }
